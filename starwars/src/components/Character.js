e38a0a9e82b443acd1b51ea8596ed243c017a248
Mirwes <mirwes210@gmail.com>
2020-09-01 16:24:12 -0700

    Completed the Sprint Challenge

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,19 @@
 // Write your Character component here
+import React from "react";
+
+const Characters = (props) => {
+console.log('props', props);
+    return (
+        <div>
+          <h3 >{props.setStarData.name}</h3>
+          <p>{props.setStarData.gender}</p>
+          <p>{props.setStarData.birth_year}</p>
+          <p>{props.setStarData.hair_color}</p>
+
+            </div>
+        
+    )
+}
+
+
+export default Characters;

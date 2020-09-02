f9ef131df6995762b5e83c3ff746ec755ea993c7
Mirwes <mirwes210@gmail.com>
2020-09-01 23:52:34 -0700

    Added some styles to the components

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,14 +1,31 @@
 // Write your Character component here
 import React from "react";
+import styled from "styled-components";
 
+
+export const Button = styled.button `
+
+background: purple;
+border: 3px solid red;
+color: lightblue;
+width: 300px;
+
+
+&:hover {
+  background: transparent;
+  color: blue;
+  }
+  `;
 const Characters = (props) => {
 console.log('props', props);
     return (
         <div>
+          <Button>
           <h3 >{props.setStarData.name}</h3>
           <p>{props.setStarData.gender}</p>
           <p>{props.setStarData.birth_year}</p>
           <p>{props.setStarData.hair_color}</p>
+          </Button>
 
             </div>
         
